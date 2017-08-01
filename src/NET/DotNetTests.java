0e47c0ddf761de2f1d107c1add4516cf54dbeb17
Bukhantsov Kostiantyn <kostiantyn.bukhantsov@hpe.com>
2017-08-01 12:07:07 +0300

    Added opportunity to pass "url"

--- src/NET/DotNetTests.java
+++ src/NET/DotNetTests.java
@@ -21,14 +21,13 @@ public class DotNetTests extends UnitTestClassBase {
     private static DotNetAppModel appModel;
     private static ProcessBuilder window;
 
-//        private static String applocation = "C:\\Users\\gadian\\Desktop\\AOS\\2017_05_24_AdvantageShopAdministrator\\Debug\\AdvantageShopAdministrator.exe";
-//    private static String applocation = "C:\\AOS\\AdvancedOnlineShopping1.1.2\\AdvantageShopAdministrator.exe";
+    //    private static String applocation = "C:\\AOS\\AdvancedOnlineShopping1.1.2\\AdvantageShopAdministrator.exe";   // Path on LeanFT Dev virtual machine
     private static String applocation = "C:\\LeanFTJavaRunner\\dotnet_release\\AdvantageShopAdministrator.exe";     // Path on CI
-//        private static String SERVER = "http://52.32.172.3:8080";
-//    private static String SERVER = "http://16.59.19.163:8080"; // LOCALHOST
-    private static String SERVER = "http://16.60.158.84:80"; // CI
-//    private static String SERVER = "http://52.88.236.171:8080"; // PRODUCTION
-
+    private static String SERVER_DEFAULT = "http://16.60.158.84:80"; // CI
+    //    private static String SERVER_DEFAULT = "http://52.88.236.171:8080"; // PRODUCTION
+    //        private static String SERVER_DEFAULT = "http://52.32.172.3:8080";
+    //    private static String SERVER_DEFAULT = "http://16.59.19.163:8080"; // LOCALHOST
+    public static String SERVER = System.getProperty("url", SERVER_DEFAULT);
 
     private static String UNAME = "Dot.NetUser";
     private static String PASSWORD = "Password1";
@@ -186,6 +185,7 @@ public class DotNetTests extends UnitTestClassBase {
     }
 
     public static void InitBeforeClass() throws IOException, GeneralLeanFtException {
+        print("CURRENT SERVER: " + SERVER);
         appModel = new DotNetAppModel();
         window = new ProcessBuilder(applocation);
         window.start();
