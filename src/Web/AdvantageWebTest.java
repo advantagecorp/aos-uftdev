d1963d334be7afe05a43a02783a2d39a6a5dcb19
Bukhantsov Kostiantyn <kostiantyn.bukhantsov@hpe.com>
2017-09-04 14:13:49 +0300

    Add sleep and twice enter cardNumber, because crashe on CI

--- src/Web/AdvantageWebTest.java
+++ src/Web/AdvantageWebTest.java
@@ -391,13 +391,15 @@ public class AdvantageWebTest extends UnitTestClassBase {
         clickWebElement(appModel.AdvantageShoppingPage().MasterCreditImage());
 
         // Set the card number
+        threadSleep(4000);
+        setValueEditField(appModel.AdvantageShoppingPage().CardNumberEditField(), cardnum);
+        threadSleep(4000);
         setValueEditField(appModel.AdvantageShoppingPage().CardNumberEditField(), cardnum);
         // Set the CVV number
         threadSleep(4000);
         setValueEditField(appModel.AdvantageShoppingPage().CvvNumberEditField(), CVV);
         threadSleep(4000);
         setValueEditField(appModel.AdvantageShoppingPage().CvvNumberEditField(), CVV);
-        threadSleep(4000);
         // Set the card holder name
         setValueEditField(appModel.AdvantageShoppingPage().CardholderNameEditField(), holdername);
         if (!save) {
