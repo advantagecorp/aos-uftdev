861ac5b4e43b6314e835a71a6a2ebfabeeb146d4
Bukhantsov Kostiantyn <kostiantyn.bukhantsov@hpe.com>
2017-09-10 22:13:47 +0300

    Web Tests update

--- src/Web/AdvantageWebTest.java
+++ src/Web/AdvantageWebTest.java
@@ -30,8 +30,8 @@ public class AdvantageWebTest extends UnitTestClassBase {
     public static String SearchURL = "";
     public static String appURL = System.getProperty("url", "defaultvalue");
 //    public static String appURL2 = "52.32.172.3";
-//	public static String appURL2 = "16.60.158.84";			// CI
-	public static String appURL2 = "16.59.19.163:8080";		// LOCALHOST
+	public static String appURL2 = "16.60.158.84";			// CI
+//	public static String appURL2 = "16.59.19.163:8080";		// LOCALHOST
 //	public static String appURL2 = "35.162.69.22:8080";		//
 //	public static String appURL2 = "156.152.164.67:8080";	//
 //	public static String appURL2 = "52.88.236.171";			// PRODUCTION
@@ -1651,7 +1651,7 @@ public class AdvantageWebTest extends UnitTestClassBase {
         Pattern r = Pattern.compile(pattern);
 
         signIn();
-        browserSync();
+        browserRefresh();
 //        threadSleep(4000);
         clickWebElement(appModel.AdvantageShoppingPage().LAPTOPSWebElement());
 
